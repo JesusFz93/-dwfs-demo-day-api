92d6c0e742eb2742c2a6a2a3148674da883230c7
Jesus <jesus.fdz93@gmail.com>
2022-09-25 13:51:27 -0500

    listo

--- src/index.js
+++ src/index.js
@@ -26,10 +26,11 @@ app.get("/", (req, res) => {
     ],
   });
 });
-app.use("/api/products", require("./routes/products.routes"));
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", require("./routes/users.routes"));
 app.use("/api/events", require("./routes/events.routes"));
+app.use("/api/reservations", require("./routes/reservations.routes"));
 
 // 4. SERVIDOR
 app.listen(PORT, () => {
